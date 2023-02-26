ea1c6a2e62319c96f0f30a1efc43b1e25b7e602f
Sabbadini10 <matiassabbadini24@gmail.com>
2023-02-26 17:41:20 -0300

    arregle responsive de banner

--- src/componentes/banner/Banner.jsx
+++ src/componentes/banner/Banner.jsx
@@ -3,11 +3,25 @@ import "./Banner.css";
 
 function Banner() {
 	return (
-		<div className='contenedor-banner mt-5 mb-1'>
+		<div className='contenedor-banner my-5'>
 			<img
 				className="banner-imagen"
-				src='https://dummyimage.com/100x300/000/fff'
+				src='https://dummyimage.com/100x300/e31212/fff'
 			/>
+			<div className="contenedor-franja-banner d-flex flex-row justify-content-around mx-1">
+				<div className="franja-banner texto-summer-hidden-1 text-center">
+				🏊‍♂️🏖️🍹 New In Summer 🍹🏖️🏊‍♂️
+				</div>
+				<div className="franja-banner texto-summer-hidden-2 ">
+				🏊‍♂️🏖️🍹 New In Summer 🍹🏖️🏊‍♂️
+				</div>
+				<div className="franja-banner texto-summer-hidden-3 ">
+				🏊‍♂️🏖️🍹 New In Summer 🍹🏖️🏊‍♂️
+				</div>
+				<div className="franja-banner texto-summer-hidden-4">
+				🏊‍♂️🏖️🍹 New In Summer 🍹🏖️🏊‍♂️
+				</div>
+			</div>
 		</div>
 	);
 }
